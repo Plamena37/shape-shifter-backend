74508deda8cb353cf044ddbdd035ed971c27c69b
Plamena <yomena37@gmail.com>
2023-11-04 21:51:27 +0200

    Code cleanup

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
-  await app.listen(process.env.DEV_PORT);
+  await app.listen(process.env.DEV_PORT || 3000);
 }
 bootstrap();
